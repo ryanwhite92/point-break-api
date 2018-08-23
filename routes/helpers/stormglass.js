f32fc6c9c5f43af0622153fbd9b176dcb242747a
Ryan White <ryanjwhite10@gmail.com>
2018-08-23 22:44:58 +0000

    Add params to api call

--- routes/helpers/stormglass.js
+++ routes/helpers/stormglass.js
@@ -2,7 +2,8 @@ const sgApiKey = process.env.SG_KEY
 
 // Returns surf data from stormglass API based on input coordinates
 function getSurfData(latitide, longitude) {
-  const stormglassUrl = `https://api.stormglass.io/point?lat=${latitude}&lng=${longitude}`;
+  const params = 'swellHeight,waveHeight';
+  const stormglassUrl = `https://api.stormglass.io/point?lat=${latitude}&lng=${longitude}&params=${params}`;
 
   fetch(stormglassUrl, {
     headers: {
