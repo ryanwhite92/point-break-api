bf1f482d0d5934e1fc4b7385f9b7f01231cd8c6a
Ryan White <ryanjwhite10@gmail.com>
2018-08-24 18:33:10 +0000

    Refactor getSurfData to use async/await

--- routes/helpers/stormglass.js
+++ routes/helpers/stormglass.js
@@ -4,22 +4,22 @@ require('isomorphic-fetch');
 const sgApiKey = process.env.SG_KEY;
 
 // Returns surf data from stormglass API based on input coordinates
-function getSurfData(latitude, longitude) {
+async function getSurfData(beach) {
+  const latitude = Number(beach.latitude);
+  const longitude = Number(beach.longitude);
   console.log("getting surf data...");
 
   const params = 'swellHeight,waveHeight';
-  let status;
 
-  fetch(`https://api.stormglass.io/point?lat=${latitude}&lng=${longitude}&params=${params}`, {
+  const response = await fetch(`https://api.stormglass.io/point?lat=${latitude}&lng=${longitude}&params=${params}`, {
     headers: {
       'Authorization': sgApiKey
     }
-  }).then(function(response) {
-    return response.json();
-  }).then(function(surfStatus) {
-    console.log(surfStatus);
-    return surfStatus;
-  }).catch((error) => console.error(error));
+  });
+  const json = await response.json();
+
+  return json;
 }
 
 module.exports = { getSurfData };
+
