d4d057c0b55ff85324d44d86a682436bab7d8de1
Ryan White <ryanjwhite10@gmail.com>
2018-08-27 22:12:45 +0000

    Add mailgun and sendEmail notification functionality

--- routes/helpers/notification.js
+++ routes/helpers/notification.js
@@ -1,8 +1,12 @@
-const twilio = require('twilio');
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = new twilio(accountSid, authToken);
-const twilioNumber = process.env.TWILIO_NUMBER
+const twilio  = require('twilio');
+const twilioNumber = process.env.TWILIO_NUMBER;
+const accountSid   = process.env.TWILIO_SID;
+const authToken    = process.env.TWILIO_AUTH_TOKEN;
+const client       = new twilio(accountSid, authToken);
+
+const mailgunKey    = process.env.MAILGUN_API_KEY;
+const mailgunDomain = process.env.MAILGUN_DOMAIN;
+const mailgun = require('mailgun-js')({ apiKey: mailgunKey, domain: mailgunDomain });
 
 function sendSMS(userNumber, beach) {
   client.messages.create({
@@ -14,4 +18,17 @@ function sendSMS(userNumber, beach) {
   .catch(error => console.error(error));
 }
 
-module.exports = { sendSMS };
+function sendEmail(userEmail, beach) {
+  const data = {
+    from: 'Admin <admin@surfbuddy.com>',
+    to: `${userEmail}`,
+    subject: 'Surf Update',
+    text: `Great surfing conditions at ${beach}`
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+  })
+}
+
+module.exports = { sendSMS, sendEmail };
