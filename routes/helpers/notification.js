92744719211797a7d28c187c20a525b53dd065f9
Ryan White <ryanjwhite10@gmail.com>
2018-08-29 21:40:44 +0000

    Move notification prep function to notification helper

--- routes/helpers/notification.js
+++ routes/helpers/notification.js
@@ -31,4 +31,35 @@ function sendEmail(userEmail, beach) {
   })
 }
 
-module.exports = { sendSMS, sendEmail };
+function prepareUserNotifications(knex) {
+  knex("beaches")
+    .join("favorites", "beaches.id", "favorites.beach_id")
+    .join("users", "users.id", "favorites.user_id")
+    .select()
+    .then((results) => {
+      console.log(results);
+      results.forEach((result) => {
+        const { email, name, stormglass } = result;
+
+        // For now only sends one email if conditions are good on one of the forecast days
+        // per beach
+        for (let i = 0; i < stormglass.length; i++) {
+          let dailyReport = stormglass[i];
+          let notificationSent = false;
+
+          for (let key of Object.keys(dailyReport)) {
+            if (dailyReport[key].surfRating >= 4) {
+              notificationSent = true;
+              sendEmail(email, name);
+              // sendSMS('+1(yourPhoneNumber)', 'Sombrio Beach');
+            }
+          }
+
+          if (notificationSent) break;
+        }
+      });
+    })
+    .catch(error => console.error(error));
+}
+
+module.exports = { prepareUserNotifications };
