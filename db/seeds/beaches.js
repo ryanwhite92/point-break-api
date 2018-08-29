4106fce6d88a70ca6208479615b179ff2085e38f
Cathleen Melendez <cathleen@melendez.com>
2018-08-29 17:59:51 +0000

    Fixed typo in Florencia Bay

--- db/seeds/beaches.js
+++ db/seeds/beaches.js
@@ -11,7 +11,7 @@ exports.seed = function(knex, Promise) {
         knex('beaches').insert({name: 'Bear Beach', latitude: 48.452520, longitude: -124.18218}),
         knex('beaches').insert({name: 'Sombrio Beach', latitude: 48.499151, longitude: -124.301541}),
         knex('beaches').insert({name: 'Port Renfrew', latitude: 48.530889, longitude: -124.466240}),
-        knex('beaches').insert({name: 'Florencia Boy', latitude: 48.993466, longitude: -125.632130}),
+        knex('beaches').insert({name: 'Florencia Bay', latitude: 48.993466, longitude: -125.632130}),
         knex('beaches').insert({name: 'Long Beach', latitude:  49.060110, longitude: -125.749646}),
         knex('beaches').insert({name: 'Cox Bay', latitude: 49.101182, longitude: -125.888953}),
         knex('beaches').insert({name: 'Chesterman Beach', latitude: 49.120379, longitude: -125.903195})
