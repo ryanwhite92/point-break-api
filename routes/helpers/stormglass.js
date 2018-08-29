9f1935091914fcfcfbf86d7ef8c82d2785412ccd
Ryan White <ryanjwhite10@gmail.com>
2018-08-29 18:59:24 +0000

    Refactor functions

--- routes/helpers/stormglass.js
+++ routes/helpers/stormglass.js
@@ -6,8 +6,7 @@ const darkskyApiKey = process.env.DARKSKY_KEY;
 
 // Returns surf data from stormglass API based on input coordinates
 async function getSurfData(beach) {
-  const latitude = Number(beach.latitude);
-  const longitude = Number(beach.longitude);
+  const { latitude, longitude } = beach;
   const params = 'waveHeight,swellHeight,wavePeriod,windSpeed,windDirection';
 
   const response = await fetch(`https://api.stormglass.io/point?lat=${latitude}&lng=${longitude}&params=${params}`, {
@@ -20,9 +19,9 @@ async function getSurfData(beach) {
   return json;
 }
 
+// Returns weather data from darksky API based on input coordinates
 async function getWeatherData(beach) {
-  const latitude = beach.latitude;
-  const longitude = beach.longitude;
+  const { latitude, longitude } = beach;
   const exclude = 'minutely,hourly,alerts,flags';
 
   const darksky = await fetch(`https://api.darksky.net/forecast/${darkskyApiKey}/${latitude},${longitude}?exclude=${exclude}&units=ca`);
@@ -32,12 +31,23 @@ async function getWeatherData(beach) {
   return dailyForecast;
 }
 
+function calcSurfRating(dailyReport) {
+  const { waveHeight, swellHeight, wavePeriod, windSpeed, windDirection } = dailyReport;
+  let surfRating = 0;
+
+  if (waveHeight && waveHeight >= 1 && waveHeight <= 3) surfRating++;
+  if (swellHeight && swellHeight >= 0.5 && swellHeight <= 3) surfRating++;
+  if (wavePeriod && wavePeriod >= 12) surfRating++;
+  if (windSpeed && windSpeed <= 15) surfRating++;
+  if (windDirection && windDirection >= 210 && windDirection <= 290) surfRating++;
+
+  return surfRating;
+}
+
 // Builds weekly surf report based on first entry of each day, using the values from the
 // first index of each array in params variable above
 async function buildSurfReport(beach) {
   const weeklyReport = [];
-  // const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
   const weatherData = await getWeatherData(beach);
   const surfData = await getSurfData(beach);
 
@@ -72,19 +82,6 @@ async function buildSurfReport(beach) {
 
   console.log(weeklyReport)
   return weeklyReport;
-}
-
-function calcSurfRating(dailyReport) {
-  const { waveHeight, swellHeight, wavePeriod, windSpeed, windDirection } = dailyReport;
-  let surfRating = 0;
-
-  if (waveHeight && waveHeight >= 1 && waveHeight <= 3) surfRating++;
-  if (swellHeight && swellHeight >= 0.5 && swellHeight <= 3) surfRating++;
-  if (wavePeriod && wavePeriod >= 12) surfRating++;
-  if (windSpeed && windSpeed <= 15) surfRating++;
-  if (windDirection && windDirection >= 210 && windDirection <= 290) surfRating++;
-
-  return surfRating;
 }
 
 module.exports = { buildSurfReport };
